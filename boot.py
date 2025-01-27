0271ccf910e9104914ff37d57122756d76e19f6f
ericbo <ericbo@users.noreply.github.com>
2025-01-26 23:20:11 -0500

    Disconnect/reconnect to network after sleep

--- boot.py
+++ boot.py
@@ -10,6 +10,7 @@ import esp
 
 esp.osdebug(None)
 import gc
+
 gc.collect()
 
 ssid = 'SSID'
@@ -19,50 +20,77 @@ temp_topic = b'bedroom/esp32/temperature'
 humid_topic = b'bedroom/esp32/humidity'
 
 wan = network.WLAN(network.WLAN.IF_STA)
-wan.active(True)
-wan.connect(ssid, password)
-
-while not wan.isconnected():
-    pass
-
-print('Connection successful')
-print(wan.ifconfig())
-
 client_id = ubinascii.hexlify(machine.unique_id())
-
-
-def connect_and_subscribe():
-  global client_id, mqtt_server
-  client = MQTTClient(client_id, mqtt_server)
-  client.connect()
-  print('Connected to %s MQTT broker!' % (mqtt_server))
-  return client
-
-def restart_and_reconnect():
-  print('Failed to connect to MQTT broker. Reconnecting...')
-  time.sleep(10)
-  machine.reset()
-
-try:
-  client = connect_and_subscribe()
-except OSError as e:
-  restart_and_reconnect()
-
-
-# Initialize the DHT22 sensor
+client = MQTTClient(client_id, mqtt_server)
 DHT22 = dht.DHT22(machine.Pin(4))
 
-# Read data from the sensor every 2 seconds
+
+def connect_to_wan():
+    wan.active(True)
+    wan.connect(ssid, password)
+
+    while not wan.isconnected():
+        pass
+
+    print('Connection successful')
+    print(wan.ifconfig())
+
+def connect_to_mqtt():
+    while True:
+        try:
+            client.connect()
+            break
+        except OSError:
+            print("Failed to connect to MQTT broker...")
+
+def disconnect_from_wan():
+    wan.disconnect()
+    wan.active(False)
+
+def disconnect_from_mqtt():
+    client.disconnect()
+
+def get_temp_and_humidity():
+    """ Get 3 good readings from sensor and return the average """
+    count = 0
+    temperature = 0
+    humidity = 0
+
+    while count < 3:
+        time.sleep(2)
+
+        try:
+            DHT22.measure()
+            temp = DHT22.temperature()
+            humid = DHT22.humidity()
+        except OSError:
+            print("Failed to read sensor, retrying")
+            continue
+
+        if not isinstance(temp, (float, int)):
+            print("Bad temperature reading, retrying")
+            continue
+
+        if not isinstance(humid, (float, int)):
+            print("Bad humidity reading, retrying")
+            continue
+
+        temperature += temp
+        humidity += humid
+        count += 1
+
+    return round(temperature / 2, 2), round(humidity / 2, 2)
+
 while True:
-    try:
-        DHT22.measure()
-        temp = DHT22.temperature()  # Gets the temperature in Celsius
-        humidity = DHT22.humidity()  # Gets the relative humidity in %
+    temp, humidity = get_temp_and_humidity()
+    print("Temperature: {:.2f}°C, Humidity: {:.2f}%".format(temp, humidity))
 
-        print("Temperature: {:.2f}°C, Humidity: {:.2f}%".format(temp, humidity))
-        client.publish(temp_topic, str(temp))
-        client.publish(humid_topic, str(humidity))
-    except OSError as e:
-        print("Failed to read from DHT22 sensor:", e)
+    connect_to_wan()
+    connect_to_mqtt()
 
-    time.sleep(2)
+    client.publish(temp_topic, str(temp))
+    client.publish(humid_topic, str(humidity))
+
+    disconnect_from_mqtt()
+    disconnect_from_wan()
+    time.sleep(10)
